52212bdc0c2fa7111aacc46f2537592a44743c7d
Lima <lmimsra07@gmail.com>
2021-02-28 21:18:08 +0900

    #3 footer分離

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="section-heading">
+        <p>
+          <strong>Bulma Personal Template</strong> by <a href="https://mubaidr.js.org">Muhammad Ubaid Raza</a>.
+          source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+        <p>
+          <strong>Customized</strong> by
+          <a href="https://twitter.com/lmimsra_dev">Lima</a>.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
