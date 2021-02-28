0fd02c8e17b389e250a7f114888e6e4b629d0df5
Lima <lmimsra07@gmail.com>
2021-02-28 20:52:40 +0900

    #3 一旦headerパーツ分離

--- pages/index.js
+++ pages/index.js
@@ -9,53 +9,11 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+
 
       <div>
-        <nav className="navbar is-link is-fixed-top">
-          <div className="navbar-brand">
-            <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-
-          <div id="navbarExampleTransparentExample" className="navbar-menu">
-            <div className="navbar-end">
-              <a className="navbar-item" href="#about">
-          <span className="icon">
-            <i className="fas fa-info"></i>
-          </span>
-                <span>About</span>
-              </a>
-              <a className="navbar-item" href="#services">
-          <span className="icon">
-            <i className="fas fa-bars"></i>
-          </span>
-                <span>Services</span>
-              </a>
-              <a className="navbar-item" href="#resume">
-          <span className="icon">
-            <i className="fas fa-file-alt"></i>
-          </span>
-                <span>Resume</span>
-              </a>
-              <a className="navbar-item" href="#portfolio">
-          <span className="icon">
-            <i className="fas fa-th-list"></i>
-          </span>
-                <span>Portfolio</span>
-              </a>
-              <a className="navbar-item" href="#contact">
-          <span className="icon">
-            <i className="fas fa-envelope"></i>
-          </span>
-                <span>Contact</span>
-              </a>
-            </div>
-          </div>
-        </nav>
+        {/*header*/}
+        <Header />
 
         <section className="hero is-link is-fullheight is-fullheight-with-navbar">
           <div className="hero-body">
