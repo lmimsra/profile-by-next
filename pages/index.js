54c5ce76d7d19737488c9a9d51dfd1e40e3da897
Lima <lmimsra07@gmail.com>
2021-02-28 22:38:29 +0900

    #5 skillTag を追加

--- pages/index.js
+++ pages/index.js
@@ -61,13 +61,13 @@ export default function Home() {
           </div>
 
           <div className="columns has-same-height is-gapless">
-            <div class="column">
-              <div class="card">
-                <div class="card-content">
-                  <h3 class="title is-4">Profile</h3>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <h3 className="title is-4">Profile</h3>
 
-                  <div class="content">
-                    <table class="table-profile">
+                  <div className="content">
+                    <table className="table-profile">
                       <tr>
                         <th colspan="1"></th>
                         <th colspan="2"></th>
@@ -87,13 +87,13 @@ export default function Home() {
                     </table>
                   </div>
                   <br/>
-                  <div class="buttons has-addons is-centered">
-                    <a href="https://github.com/lmimsra" class="button is-link">
+                  <div className="buttons has-addons is-centered">
+                    <a href="https://github.com/lmimsra" className="button is-link">
                       Github
                     </a>
                     <a
                       href="https://twitter.com/lmimsra_dev"
-                      class="button is-link"
+                      className="button is-link"
                     >
                       Twitter
                     </a>
@@ -117,6 +117,15 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        {/* skill tags*/}
+        <div className="tags custom-tags">
+          {
+            getSkillTags().map((val)=>
+              <span className="tag is-light">{val}</span>
+            )
+          }
+        </div>
 
         <section className="section" id="services">
           <div className="section-heading">
@@ -646,5 +655,91 @@ function getSkillPoints() {
     {name: 'Dart(Flutter)', point: 10},
     {name: 'Go', point: 10},
     {name: 'Database(MySQL)', point: 45}
+  ]
+}
+
+// スキルタグの値取得
+function getSkillTags() {
+  return [
+    'Node.js',
+    'VueJS',
+    'JavaScript',
+    'HTML5',
+    'CSS3',
+    'Bulma',
+    'Bootstrap',
+    'PHP',
+    'Laravel',
+    'MySQL',
+    'AWS',
+    'Go',
+    'Flutter',
+    'PHPUnit',
+    'Git',
+    'RESTful API',
+    'JetBrains',
+    'Nginx',
+    'firebase',
+    'serverless'
+  ]
+}
+
+// 経験の値取得
+function getResumeData() {
+  return [
+    {
+      title: 'スマホ予約アプリAPI開発',
+      about: '大手飲食店の席予約アプリ運用開発',
+      technology: 'Lumen(php7.0), Redis, MySQL, EC2(AWS)',
+      role:
+        '運用保守案件の要件定義, 設計, 実装, テスト, アプリ開発のベンダコントロール',
+      performance:
+        'バックエンド開発リード, 既存バグの改修, 案件レベルの要件定義, アプリ用の管理画面新規開発',
+      members: '6人(PM1, バックエンド2, ネイティブ3)'
+    },
+    {
+      title: '商品注文SPA API開発',
+      about: '大手飲食店の注文アプリ新規開発・運用',
+      technology:
+        'vue.js, Laravel(php7.3), RedisCluster, MySQL, Docker, ECS(AWS)',
+      role:
+        '新規開発の要件定義, 設計, 実装, テスト, 運用保守全般, ' +
+        'アプリ開発のベンダコントロール, ネイティブ, web, API横断的な開発サポート',
+      performance:
+        'PM, PL, バックエンド開発リード, 新規API設計90本(PJ合計), API実装, 運用保守対応全般, cloudWatchを使ったログ調査',
+      members: '10人(PM1, バックエンド3, ネイティブ3, フロント2, 企画1)'
+    },
+    {
+      title: '飲食店向けオウンドアプリ開発',
+      about: '大手飲食チェーン向けのオウンドアプリ新規開発',
+      technology:
+        'bootstrap, Laravel(php7.4), RedisCluster, MySQL, Docker, ECS(AWS)',
+      role: '新規開発の要件定義, アーキテクチャ設計, API概要設計',
+      performance:
+        'アプリケーション全体の要件定義, サーバーサイド技術選定, サーバーサイド全体のアーキテクチャ設計, RESTFullなAPI設計',
+      members: '8人(PM1, バックエンド3, ネイティブ3, フロント1)'
+    },
+    {
+      title: '飲食店向けスマホ注文アプリ web拡張',
+      about:
+        '飲食チェーンを対象としたスマートフォン注文アプリのネイティブ版からweb対応する機能拡張',
+      technology:
+        'vue.js, Laravel(php7.3), RedisCluster, MySQL, Docker, ECS(AWS)',
+      role: '要件定義, 設計, 実装, テスト, アーキテクチャ設計',
+      performance:
+        'バックエンド開発全体のリード, タスク管理, 機能拡張に伴う認証周辺の詳細設計・実装',
+      members: '7人(PM1, バックエンド2, ネイティブ2, フロント1, 企画1)'
+    },
+    {
+      title: '問診票システム開発',
+      about: '病院向け問診票電子化プロジェクト(大学在学中)',
+      technology: 'JavaEE(Java8), JSF, Jax-rs, MySQL, Tomcat',
+      role:
+        '要求整理, 要件定義, 設計, デザイン(CSS), 実装, ' +
+        'テスト, システム全体のUI, UX対応',
+      performance:
+        'プロトタイプ型開発提案, 開発環境構築全般, 画面デザイン・調整, 進捗管理',
+      members: '3人(PM1, エンジニア2)'
+    }
   ]
 }
