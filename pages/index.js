b25d921746ba57a6d54613bd48ca6caa5f4fbab5
Lima <lmimsra07@gmail.com>
2021-02-28 23:49:53 +0900

    #6 ついでにコンタクトのところを良い感じに削った

--- pages/index.js
+++ pages/index.js
@@ -183,54 +183,54 @@ export default function Home() {
           <div className="container">
             <div className="section-heading">
               <h3 className="title is-2">Contact</h3>
-              <h4 className="subtitle is-5">Get in touch</h4>
+              <h4 className="subtitle is-5">Twitterかメールでよろしくです！</h4>
+              <h4 className="subtitle is-5">（フォーム準備できずすいません）</h4>
             </div>
             <br/>
 
-            <div className="columns">
-              <div className="column is-6 is-offset-3">
-                <div className="box">
-                  <div className="field">
-                    <label className="label">Name</label>
-                    <div className="control">
-                      <input className="input" type="text" placeholder="Text input"/>
-                    </div>
-                  </div>
+            {/*<div className="columns">*/}
+            {/*  <div className="column is-6 is-offset-3">*/}
+            {/*    <div className="box">*/}
+            {/*      <div className="field">*/}
+            {/*        <label className="label">Name</label>*/}
+            {/*        <div className="control">*/}
+            {/*          <input className="input" type="text" placeholder="Text input"/>*/}
+            {/*        </div>*/}
+            {/*      </div>*/}
 
-                  <div className="field">
-                    <label className="label">Email</label>
-                    <div className="control has-icons-left">
-                      <input className="input" type="email" placeholder="Email input" value=""/>
-                      <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-                    </div>
-                  </div>
+            {/*      <div className="field">*/}
+            {/*        <label className="label">Email</label>*/}
+            {/*        <div className="control has-icons-left">*/}
+            {/*          <input className="input" type="email" placeholder="Email input" value=""/>*/}
+            {/*          <span className="icon is-small is-left">*/}
+            {/*      <i className="fas fa-envelope"></i>*/}
+            {/*    </span>*/}
+            {/*        </div>*/}
+            {/*      </div>*/}
 
-                  <div className="field">
-                    <label className="label">Message</label>
-                    <div className="control">
-                      <textarea className="textarea" placeholder="Textarea"></textarea>
-                    </div>
-                  </div>
+            {/*      <div className="field">*/}
+            {/*        <label className="label">Message</label>*/}
+            {/*        <div className="control">*/}
+            {/*          <textarea className="textarea" placeholder="Textarea"></textarea>*/}
+            {/*        </div>*/}
+            {/*      </div>*/}
 
-                  <div className="field is-grouped has-text-centered">
-                    <div className="control">
-                      <button className="button is-link is-large"><span className="icon">
-                    <i className="fas fa-envelope"></i>
-                  </span>
-                        <span>Submit</span></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {/*      <div className="field is-grouped has-text-centered">*/}
+            {/*        <div className="control">*/}
+            {/*          <button className="button is-link is-large"><span className="icon">*/}
+            {/*        <i className="fas fa-envelope"></i>*/}
+            {/*      </span>*/}
+            {/*            <span>Submit</span></button>*/}
+            {/*        </div>*/}
+            {/*      </div>*/}
+            {/*    </div>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
 
           </div>
         </section>
 
-        {/*footer*/
-        }
+        {/*footer*/}
         <Footer/>
       </div>
     </div>
